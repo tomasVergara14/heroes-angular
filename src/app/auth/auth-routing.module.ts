9be800dfe600ab503630feb2ebddf3c376150ea2
Tom <tomasvergara272@gmail.com>
2023-07-17 20:58:26 -0300

    auth children routes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
   {
     path:'',
-    component: LayoutPageComponent
+    component: LayoutPageComponent,
+    children:[
+      { path:'login', component: LoginPageComponent },
+      { path:'register', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'login' }
+    ]
   }
 ]
 
