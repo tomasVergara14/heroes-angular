e5ae6c349df6a3e40b3d59b9d9ee9bbaddc8c807
Tom <tomasvergara272@gmail.com>
2023-07-17 23:11:32 -0300

    auth html

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+//Modules
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+//Components
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
@@ -11,11 +14,12 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   declarations: [
     LayoutPageComponent,
     LoginPageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
   ],
   imports: [
+    AuthRoutingModule,
     CommonModule,
-    AuthRoutingModule
+    MaterialModule,
   ]
 })
 export class AuthModule { }
