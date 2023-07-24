65283e8a7b838f04c29732695abb30d3b11c901e
Tom <tomasvergara272@gmail.com>
2023-07-23 21:25:22 -0300

    new hero html

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public publishers = [
+    {
+      id:'DC Comics',
+      description: 'DC - Comics'
+    },
+    {
+      id:'Marvel Comics',
+      description: 'Marvel - Comics'
+    }
+  ]
+
 }
