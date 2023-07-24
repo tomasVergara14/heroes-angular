e0cc14fdbe4548d791a0b263082c0b604304077e
Tom <tomasvergara272@gmail.com>
2023-07-24 00:03:42 -0300

    crud

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 //Imports
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, of, map } from 'rxjs';
 import { environments } from 'src/environments/environments';
 //Interfaces
 import { Hero } from '../interfaces/hero.interface';
@@ -27,7 +27,25 @@ export class HeroesService {
     }
 
     getSuggestions( query: string ):Observable<Hero[]>{
-        return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}`)
+        return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}`);
     }
+
+    addHero( hero: Hero): Observable<Hero>{
+        return this.httpClient.post<Hero>(`${this.baseUrl}/heroes/`, hero);
+    }
+
+    updateHero( hero: Hero):Observable<Hero>{
+        if(!hero.id) throw Error('Hero id is required')
+        return this.httpClient.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+    }
+
+    deleteHero( id: string):Observable<boolean>{
+        return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`)
+            .pipe(
+                catchError( err => of(false)),
+                map(resp => true)
+            );
+    }
+
     
 }
